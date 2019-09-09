d6ae547a34ffd91aca5f442d36992e399fce292b
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-09-09 13:26:51 +0200

    fix(no-script): check if process variable exists before using it

--- src/no-script.vue
+++ src/no-script.vue
@@ -1,7 +1,7 @@
 <script>
 export default {
   render (createElement) {
-    if (process.browser) {
+    if (typeof process !== 'undefined' && process.browser) {
       return createElement('noscript')
     } else {
       return createElement('noscript', this.$slots.default)
