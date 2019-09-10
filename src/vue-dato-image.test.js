9b589022eda127707e65063925a3d0ea61956c22
Sjoerd Beentjes <mail@sjoerdbeentjes.nl>
2019-09-10 10:05:18 +0200

    add tests for vue-dato-image.vue

--- src/vue-dato-image.test.js
+++ src/vue-dato-image.test.js
@@ -5,13 +5,44 @@ test('Renders', () => {
   const wrapper = shallowMount(VueDatoImage, {
     propsData: { 
       image: {
-      url: 'https://www.datocms-assets.com/6524/1559739750-quantum-inspire-editor.jpg',
-      width: 1496,
-      height: 932,
-      format: 'jpg'
+        url: 'https://www.datocms-assets.com/6524/1559739750-quantum-inspire-editor.jpg',
+        width: 1496,
+        height: 932,
+        format: 'jpg'
       }
     }
   })
 
   expect(wrapper.exists()).toBe(true)
+})
+
+test('Renders picture element when image is bitmap', () => {
+  const wrapper = shallowMount(VueDatoImage, {
+    propsData: { 
+      image: {
+        url: 'https://www.datocms-assets.com/6524/1559739750-quantum-inspire-editor.jpg',
+        width: 1496,
+        height: 932,
+        format: 'jpg'
+      }
+    }
+  })
+
+  expect(wrapper.find('picture').isVisible()).toBe(true)
+})
+
+test('Renders img element when image is vector', () => {
+  const wrapper = shallowMount(VueDatoImage, {
+    propsData: {
+      image: {
+        width: null,
+        height: null,
+        format: 'svg',
+        url: 'https://www.datocms-assets.com/6524/1543322842-heroes.svg',
+        alt: null
+      }
+    }
+  })
+
+  expect(wrapper.find('img').isVisible()).toBe(true)
 })
