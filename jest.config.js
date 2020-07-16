dd75349958c823be26fbdf3239925e4eaec817c4
Jasper Moelker <j.b.moelker@activeids.nl>
2020-07-16 16:09:40 +0200

    chore: update dependencies 2020-07
    Also resolve issues caused by breaking updates.
    
    Resolves #59
    Resolves #61
    Resolves #62
    Resolves #63
    Resolves #64
    Resolves #65
    Resolves #66
    Resolves #67
    Resolves #68
    Resolves #69
    Resolves #70
    Resolves #71

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  "moduleFileExtensions": [
-    "js",
-    "json",
-    "vue"
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue',
   ],
-  testEnvironment: "jest-environment-jsdom",
-  "transform": {
-    ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest",
-    "^.+\\.js$": "<rootDir>/node_modules/babel-jest"
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
   },
 };
